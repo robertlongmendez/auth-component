bd37d54eeed688dca4b1e319bc1a51db12f75004
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-06-10 17:13:42 -0500

    sending login requests

---
+++ auth-01-auth-component/src/app/auth/auth.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDNDDhL-gHvXJ7Xs4fGplw8SI5zdOjgpf0',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+
+    }
+    ).pipe(catchError(errorRes => {
+      let errorMessage = 'An unknown error occurred!'
+      if (!errorRes.error || !errorRes.error.error) {
+        return throwError(errorMessage);
+      }
+      switch (errorRes.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email exists already';
+        }
+        return throwError(errorMessage);
+    }));
+  }
+
+  login(email: string, password: string) {
+   return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDNDDhL-gHvXJ7Xs4fGplw8SI5zdOjgpf0',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    }
+    );
+  }
+}
